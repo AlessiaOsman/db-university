503f33348ea37fb518322f1412738bf506e31357
AlessiaOsman <alessia.osman@gmail.com>
2024-03-01 15:24:40 +0100

    group_by_2

--- group_by/solution.sql
+++ group_by/solution.sql
@@ -3,5 +3,7 @@
         SELECT COUNT(*) as `numero_studenti`, YEAR(`enrolment_date`) as `anno_di_iscrizione` FROM `students` GROUP BY `anno_di_iscrizione`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+        SELECT COUNT(*) AS `teachers_number`, `office_address` AS `office` FROM `teachers` GROUP BY `office`;
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
