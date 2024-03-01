45cc8165c631fda0765df0a433ed668c417d0885
AlessiaOsman <alessia.osman@gmail.com>
2024-03-01 15:47:25 +0100

    group_by_4

--- group_by/solution.sql
+++ group_by/solution.sql
@@ -1,13 +1,15 @@
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
-        SELECT COUNT(*) as `numero_studenti`, YEAR(`enrolment_date`) as `anno_di_iscrizione` FROM `students` GROUP BY `anno_di_iscrizione`;
+        SELECT YEAR(`enrolment_date`) as `anno_di_iscrizione`, COUNT(*) as `numero_studenti` FROM `students` GROUP BY `anno_di_iscrizione`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
-        SELECT COUNT(*) AS `teachers_number`, `office_address` AS `office` FROM `teachers` GROUP BY `office`;
+        SELECT `office_address` AS `office`, COUNT(*) AS `teachers_number`  FROM `teachers` GROUP BY `office`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
-        SELECT ROUND(AVG(`vote`)) as `vote_everage`, `exam_id` FROM `exam_student` WHERE `vote` >= 18 GROUP BY `exam_id`;
-        
+        SELECT `exam_id`, ROUND(AVG(`vote`)) as `vote_everage` FROM `exam_student` WHERE `vote` >= 18 GROUP BY `exam_id`;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+        SELECT `department_id` as `department`, COUNT(`id`) AS `degrees_number` FROM `degrees` GROUP BY `department`;
