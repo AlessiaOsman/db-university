9a95f22ed3d07b6b324596379c1d9e59137e1160
AlessiaOsman <alessia.osman@gmail.com>
2024-03-04 15:50:25 +0100

    join 4

--- join/join.sql
+++ join/join.sql
@@ -21,8 +21,15 @@
     JOIN `teachers` AS T ON `CT`.`teacher_id` = `T`.`id` 
     WHERE `T`.`name` = 'Fulvio' 
     AND `T`.`surname` = 'Amato';
-    
+
 -- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+     SELECT `S`.`surname` AS 'Cognome Studente', `S`.`name` AS 'Nome Studente', DEG.*, `DEP`.`name` AS 'Nome Dipartimento' 
+     FROM `students` AS S 
+     JOIN `degrees` AS DEG ON `S`.`degree_id` = `DEG`.`id` 
+     JOIN `departments` AS DEP ON `DEG`.`department_id` = `DEP`.`id` 
+     ORDER BY `S`.`surname`;
+     
 -- Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
